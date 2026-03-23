da4b504c885d51a1d721396796ed2a1d6bc29382
sesema98 <sergioserva14@gmail.com>
2026-03-23 11:23:49 -0500

    Caso 2 Implementado completamente

--- MyPlayground.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -2,6 +2,33 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+func calcularCalorias(actividad: String, tiempo: Int) -> Double {
+    
+    if actividad == "dormir" {
+        return Double(tiempo) * 1.08
+    } else {
+        return Double(tiempo) * 1.66
+    }
+}
+
+let actividad = "reposo"
+let tiempo = 180
+
+if actividad != "dormir" && actividad != "reposo" {
+    
+    print("Error: Actividad inválida. Solo se permite 'dormir' o 'reposo'.")
+    
+} else if tiempo <= 0 {
+    
+    print("Error: El tiempo debe ser un número entero positivo.")
+    
+} else {
+    
+    let calorias = calcularCalorias(actividad: actividad, tiempo: tiempo)
+    
+    print("Actividad: \(actividad)")
+    print("Tiempo: \(tiempo) minutos")
+    print("Calorías consumidas: \(calorias)")
+}
 
 //: [Next](@next)
